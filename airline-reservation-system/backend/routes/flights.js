65aca123c3941a932cc348aad5e49c895bf1cab6
MarioAbdelmalek <mario_beshai@yahoo.com>
2021-10-30 20:34:45 +0200

    Added React Folders

--- airline-reservation-system/backend/routes/flights.js
+++ airline-reservation-system/backend/routes/flights.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Flight = require('../models/flight');
 
 /* GET home page. */
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
     res.status(200).send("You have everything installed !");
 });
 
@@ -27,21 +27,37 @@ router.post('/create', async (req, res) => {
         EconomySeats,
         BusinessClassSeats,
         DepartureAirport,
-        ArrivalAirport,
+        ArrivalAirport
     });
+    const flightTest = await newFlight.save();
+    console.log(flightTest);
 
-    try {
-        await newFlight.save();
-    }
-    catch (err) {
-        console.log("Error!", err)
-    }
+
 });
 
 
-router.get('/yarab', async (req, res) => {
-    const flights = await Flight.find({});
-    console.log(flights);
-});
+// router.get('/List', async (req, res) => {
+//     const flights = await Flight.find({});
+//     console.log(flights);
+// });
+
+// router.post('/createTrial', async (req, res) => {
+
+//     const newFlight = new Flight({
+//         FlightNo: 890,
+//         DepartureDate: new Date(),
+//         ArrivalDate: new Date(),
+//         DepartureTime: '13:20',
+//         ArrivalTime: '18:20',
+//         EconomySeats: 80,
+//         BusinessClassSeats: 15,
+//         DepartureAirport: "CAI",
+//         ArrivalAirport: "JFL=K"
+//     });
+
+//     await newFlight.save();
+
+// });
+
 
 module.exports = router;
