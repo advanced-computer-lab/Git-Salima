8076ada498836a17cec372752fee2e657d4dfd7b
Philopateer Fouad <Philopateeramgad17@hotmail.com>
2021-10-31 23:27:59 +0200

    All Flights Display

--- airline-reservation-system/backend/routes/flights.js
+++ airline-reservation-system/backend/routes/flights.js
@@ -1,53 +1,48 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Flight = require('../models/flight');
+const Flight = require("../models/flight");
 router.use(express.urlencoded({ extended: true }));
 
 /* GET home page. */
-router.get('/', (req, res) => {
-    res.status(200).send("You have everything installed !");
+router.get("/", (req, res) => {
+  res.status(200).send("You have everything installed !");
 });
 
-router.post('/create', async (req, res) => {
-    const FlightNo = Number(req.body.FlightNo);
-    const DepartureDate = Date.parse(req.body.DepartureDate);
-    const ArrivalDate = Date.parse(req.body.ArrivalDate);
-    const DepartureTime = req.body.DepartureTime;
-    const ArrivalTime = req.body.ArrivalTime;
-    const EconomySeats = Number(req.body.EconomySeats);
-    const BusinessClassSeats = Number(req.body.BusinessClassSeats);
-    const DepartureAirport = req.body.DepartureAirport;
-    const ArrivalAirport = req.body.ArrivalAirport;
-    console.dir(req.body);
- 
+router.post("/create", async (req, res) => {
+  const FlightNo = Number(req.body.FlightNo);
+  const DepartureDate = Date.parse(req.body.DepartureDate);
+  const ArrivalDate = Date.parse(req.body.ArrivalDate);
+  const DepartureTime = req.body.DepartureTime;
+  const ArrivalTime = req.body.ArrivalTime;
+  const EconomySeats = Number(req.body.EconomySeats);
+  const BusinessClassSeats = Number(req.body.BusinessClassSeats);
+  const DepartureAirport = req.body.DepartureAirport;
+  const ArrivalAirport = req.body.ArrivalAirport;
+  console.dir(req.body);
 
-    const newFlight = new Flight({
-        FlightNo,
-        DepartureDate,
-        ArrivalDate,
-        DepartureTime,
-        ArrivalTime,
-        EconomySeats,
-        BusinessClassSeats,
-        DepartureAirport,
-        ArrivalAirport
-    });
-    
-    const flightTest = await newFlight.save();
-    
-  
+  const newFlight = new Flight({
+    FlightNo,
+    DepartureDate,
+    ArrivalDate,
+    DepartureTime,
+    ArrivalTime,
+    EconomySeats,
+    BusinessClassSeats,
+    DepartureAirport,
+    ArrivalAirport,
+  });
 
-
+  const flightTest = await newFlight.save();
 });
 
-
-router.get('/List', async (req, res) => {
-    const flights = await Flight.find({});
-    console.log(flights);
-    res.send(flights);
+router.get("/List", async (req, res) => {
+  const flights = await Flight.find({});
+  console.log(flights);
+  res.send(flights);
 });
 
-
-
+router.get("/Tickets", async (req, res) => {
+  res.send("opa");
+});
 
 module.exports = router;
