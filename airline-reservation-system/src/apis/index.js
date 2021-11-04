01996e6f49916c57fce47a4ad13f4e8215bfcbe9
MarioAbdelmalek <mario_beshai@yahoo.com>
2021-11-05 01:58:11 +0200

    "View All Flights"

--- airline-reservation-system/src/apis/index.js
+++ airline-reservation-system/src/apis/index.js
@@ -13,18 +13,19 @@ export const searchFlightsAPI = async (flight) => {
     .then((res) => {
       return res.data;
     });
+}
 
-  export const deleteFlightsAPI = async (flight) => {
-    //console.log(JSON.stringify(flight));
-    const temp = JSON.stringify(flight);
-    const temp2 = JSON.parse(temp);
-    return await axios.get("http://localhost:8000/delete", { params: temp2 }).then((res) => {
+export const deleteFlightsAPI = async (flight) => {
+  //console.log(JSON.stringify(flight));
+  const temp = JSON.stringify(flight);
+  const temp2 = JSON.parse(temp);
+  return await axios.get("http://localhost:8000/delete", { params: temp2 }).then((res) => {
 
 
-      return res.data;
-    });
-  };
+    return res.data;
+  });
 };
+
 // export const viewAllFlightsAPI = async () => {
 //   return await axios.get("http://localhost:8000/list")
 //     .then((res) => {
