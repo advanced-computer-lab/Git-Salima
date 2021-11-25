f7da61b98f5e8f0867b61e4157df76480b9670ff
philo-atef <phiko.atef@gmail.com>
2021-11-25 16:17:17 +0200

    added terminal and luggage

--- airline-reservation-system/src/components/search-results.js
+++ airline-reservation-system/src/components/search-results.js
@@ -35,9 +35,13 @@ const SearchResults = (props) => {
               ArrivalDate={flight.ArrivalDate}
               DepartureTime={flight.DepartureTime}
               ArrivalTime={flight.ArrivalTime}
+              Terminal={flight.Terminal}
               EconomySeats={flight.EconomySeats}
               BusinessClassSeats={flight.BusinessClassSeats}
               FirstClassSeats={flight.FirstClassSeats}
+              EconomyLuggage={flight.EconomyLuggage}
+              BusinessClassLuggage={flight.BusinessClassLuggage}
+              FirstClassLuggage={flight.FirstClassLuggage}
               DepartureAirport={flight.DepartureAirport}
               ArrivalAirport={flight.ArrivalAirport}
 
