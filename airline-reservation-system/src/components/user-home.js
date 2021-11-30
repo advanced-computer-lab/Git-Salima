e1a853a495bf92c9464304c7a59f6fe37c8b70fa
MarioAbdelmalek <mario_beshai@yahoo.com>
2021-11-30 19:55:31 +0200

    enter

--- airline-reservation-system/src/components/user-home.js
+++ airline-reservation-system/src/components/user-home.js
@@ -84,7 +84,6 @@ const Flight = () => {
   return (
     <div>
       <ThemeProvider theme={theme}>
-<<<<<<< HEAD
         <div class="box">
           <img class="img" src={bg} alt="Girl in a jacket" />
         </div>
@@ -96,112 +95,7 @@ const Flight = () => {
           {" "}
           We Git you where you want to go
         </Typography>
-        {/* <Box
-          component="form"
-          sx={{
-            "& .MuiTextField-root": { m: 1, width: "25ch" },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <div>
-            <Autocomplete
-              id="auto-complete"
-              autoComplete
-              includeInputInList
-              options={allFlights}
-              getOptionLabel={(option) => option.DepartureAirport.toString()}
-              sx={{ width: 300 }}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  label="Departure Airport"
-                  variant="standard"
-                />
-              )}
-            />
 
-            <TextField
-              required
-              id="filled-required"
-              label="Departure Airport"
-              defaultValue="Departure Airport"
-              variant="filled"
-              value={DepartureAirport}
-              onChange={(e) => setDepartureAirport(e.target.value)}
-            />
-            <TextField
-              required
-              id="filled-required"
-              label="Arrival Airport"
-              defaultValue="Arrival Airport"
-              variant="filled"
-              value={ArrivalAirport}
-              onChange={(e) => setArrivalAirport(e.target.value)}
-            />
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <DatePicker
-                label="Departure Date"
-                value={DepartureDate}
-                onChange={(newValue) => {
-                  setDepartureDate(newValue);
-                }}
-                renderInput={(params) => <TextField {...params} />}
-              />
-            </LocalizationProvider>
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <DatePicker
-                label="Return Date"
-                value={ArrivalDate}
-                onChange={(newValue) => {
-                  setArrivalDate(newValue);
-                }}
-                renderInput={(params) => <TextField {...params} />}
-              />
-            </LocalizationProvider>
-            <TextField
-              id="filled-number"
-              label="Number of Adults"
-              type="number"
-              defaultValue="1"
-              InputProps={{ inputProps: { min: 1 } }}
-              InputLabelProps={{
-                shrink: true,
-              }}
-              variant="filled"
-            />
-            <TextField
-              id="filled-number"
-              label="Number of Children"
-              type="number"
-              defaultValue="0"
-              InputProps={{ inputProps: { min: 0 } }}
-              InputLabelProps={{
-                shrink: true,
-              }}
-              variant="filled"
-            />
-            <FormControl sx={{ m: 1, minWidth: 120 }}>
-              <InputLabel id="demo-simple-select-label">
-                Flight Class
-              </InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={FlightClass}
-                label="Class"
-                onChange={handleChange}
-              >
-                <MenuItem value={"Economy"}>Economy</MenuItem>
-                <MenuItem value={"Business"}>Business</MenuItem>
-                <MenuItem value={"First Class"}>First Class</MenuItem>
-              </Select>
-            </FormControl>
-          </div>
-          <ColorButton variant="contained" onClick={searchFlight}>
-            Search
-          </ColorButton>
-        </Box> */}
         <Header
           color="transparent"
           brand="Git Salima Airlines"
@@ -214,7 +108,7 @@ const Flight = () => {
         />
         <br />
         <div class="card">
-          <Typography variant="h2">Search Flight</Typography>
+          <Typography variant="h2">Flight Search</Typography>
           <Card>
             <CardContent style={{ backgroundColor: "#EFEAE4" }}>
               <form onSubmit={searchFlight}>
@@ -230,24 +124,6 @@ const Flight = () => {
                       onChange={(e) => setDepartureAirport(e.target.value)}
                     />
                   </div>
-=======
-        <Typography variant="h2">Flight Search</Typography>
-        <Card>
-          <CardContent style={{ backgroundColor: "#EFEAE4" }}>
-            <form onSubmit={searchFlight}>
-              <div className="row">
-                <div className="col">
-                  <TextField
-                    required
-                    id="filled-required"
-                    label="Departure Airport"
-                    defaultValue="Departure Airport"
-                    variant="filled"
-                    value={DepartureAirport}
-                    onChange={(e) => setDepartureAirport(e.target.value)}
-                  />
-                </div>
->>>>>>> 6c264b36bb3686c19071f07a632fc2e9dc7e8f92
 
                   <div className="col-md-10">
                     <TextField
