b78776ac66a793b0736a549675bf2703be2e3775
MarioAbdelmalek <mario_beshai@yahoo.com>
2021-11-30 19:51:40 +0200

    escape

--- airline-reservation-system/src/components/user-flight-card.js
+++ airline-reservation-system/src/components/user-flight-card.js
@@ -18,10 +18,7 @@ const theme = createTheme({
 
   palette: {
     primary: {
-      // light: will be calculated from palette.primary.main,
       main: "#082567",
-      // dark: will be calculated from palette.primary.main,
-      // contrastText: will be calculated to contrast with palette.primary.main
     },
     secondary: {
       main: "#FBB404",
